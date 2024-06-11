f922d8e8c88600718f8c3484ff9fed495663b31c
blue <nobfi517@gmail.com>
2024-06-10 22:32:49 -0500

    Level path lerping

--- Modules/level.lua
+++ Modules/level.lua
@@ -1,17 +1,20 @@
 Level = {}
-Level.Path = {}
+Level.name = ""
+Level.Path = PathTbl.new()
 
-function Level:LerpPath(Obj)
+function Level:Lerp(Obj)
 	
 end
 
-function Level.new(P)
+function Level.new(n)
 	local tbl = {
-		
+		name = n or "",
+		Path = PathTbl.new()
 	}
 	
 	local mt = {
-		
+		__index = Level,
+		__call = Level.Lerp,
 	}
 	
 	return setmetatable(tbl,mt)
---
+++ Modules/path.lua
@@ -0,0 +1,63 @@
+Path = {}
+
+function Path.new(x1,x2,y1,y2,tt)
+	local tbl = {
+		x1 = x1 or 0, x2 = x2 or 0,
+		y1 = y1 or 0, y2 = y2 or 0,
+		tt = tt or 0,
+	}
+	
+	local mt = {
+		__index = Path,
+		__call = Path.Lerp,
+	}
+	
+	return setmetatable(tbl,mt)
+end
+
+function Path:Lerp(t,o)
+	o = o or 0
+	dt = math.max(0,math.min(1,(t - o)/(self.tt)))
+	return {
+		x = ((self.x1)*(1-dt)) + ((self.x2)*(dt)),
+		y = ((self.y1)*(1-dt)) + ((self.y2)*(dt)),
+	}
+end
+
+PathTbl = {}
+
+function PathTbl.new()
+	local tbl = {
+		tbl = {},
+	}
+	
+	local mt = {
+		__index = PathTbl,
+		__call = PathTbl.Lerp,
+	}
+	
+	return setmetatable(tbl,mt)
+end
+
+function PathTbl:Insert(x1,x2,y1,y2,tt)
+	table.insert(self.tbl,Path.new(x1,x2,y1,y2,tt))
+end
+
+function PathTbl:Lerp(t)
+	local n = 0
+	local c = n
+	local f = 1
+	
+	-- should be a until loop tbh
+	for i,v in ipairs(self.tbl) do
+		if t >= c then
+			c = c + v.tt
+			if t <= c then
+				f = i
+				n = c - v.tt
+			end
+		end
+	end
+	
+	return self.tbl[f](t,n)
+end
