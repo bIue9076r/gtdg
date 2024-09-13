9e9e6d7a4020f7c3e52b27804145aabfa5b7ed3d
blue <nobfi517@gmail.com>
2024-09-12 23:40:10 -0500

    multi tower frame

--- Modules/towers.lua
+++ Modules/towers.lua
@@ -130,15 +130,42 @@ Towers[3].Act = function(self,pathTbl)
 	end
 end
 
---[[
 Towers[4].Act = function(self,pathTbl)
-	
-	self.Act = function(self)
+	--self.vars["cpath"] = pathTbl:GetClosest(self)
+	self.vars.damage = 30
+	self.vars.bonus = self.vars.damage * 5
+	self.Act = function(self,objTbl)
 		-- main loop
-		print("loop")
+		self.vars.radius = Player.Tower_Radius
+		self.vars.cooldown = secondsToTicks(Player.Tower_Timer)
+		local o = objTbl:GetClosestInRId(self,nil,self.vars.radius)
+		if o then
+			--[[
+			if self.t:get() >= self.vars.cooldown then
+				o:Hit(self.vars.damage)
+				AddCash(self.vars.bonus)
+				self.t:set(0)
+				if o.hp <= 0 then
+					Player.Kills = Player.Kills + 1
+				end
+				
+				-- create bullet object
+				local v = {
+					(o.x - self.x)/d,
+					(o.y - self.y)/d
+				}
+				
+				objTbl:InsertObj(Bullet.new(self.x,self.y,v,d))
+			end
+			]]
+		end
+		if self.t:get() < self.vars.cooldown then
+			self.t()
+		end
 	end
 end
 
+--[[
 Towers[5].Act = function(self,pathTbl)
 	
 	self.Act = function(self)
